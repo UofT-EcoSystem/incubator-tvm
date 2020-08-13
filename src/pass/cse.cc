0bb93df84af27c9c397162b461242f6ecca96f68
ArmageddonKnight <bojian.zheng@mail.utoronto.ca>
2020-08-13 04:59:29 -0400

    Added an extra microbenchmark.

--- src/pass/cse.cc
+++ src/pass/cse.cc
@@ -126,6 +126,7 @@ void CSE(const Tensor & src, Tensor * const ptgt)
         IRComparator cmp;
         LOG(INFO) << "x + y == y + x?: " << cmp.Compare(x + y, y + x);
         LOG(INFO) << "x + y == y * x?: " << cmp.Compare(x + y, y * x);
+        LOG(INFO) << "x + y == y * x?: " << cmp.Compare(x * y + y, y + y * x);
 
 
         std::queue < Tensor > worklist;
