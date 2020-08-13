ec796a3cd59b449d6f5e7f6966d3aa7db095954e
ArmageddonKnight <bojian.zheng@mail.utoronto.ca>
2020-08-13 17:20:42 -0400

    Moved IRComparator to the source file.

--- src/pass/cse.cc
+++ src/pass/cse.cc
@@ -1,4 +1,5 @@
 #include <tvm/expr.h>
+#include <tvm/ir_visitor.h>
 #include <tvm/operation.h>
 
 #include "./cse.h"
@@ -19,6 +20,41 @@
 
 namespace tvm {
 namespace ir {
+
+
+class IRComparator
+{
+private:
+        bool _Compare(const Array < Expr > & lhs, const Array < Expr > & rhs);
+        bool _Compare(const IterVar & lhs, const IterVar & rhs);
+        bool _Compare(const Array < IterVar > & lhs,
+                      const Array < IterVar > & rhs);
+public:
+        ~IRComparator() {}
+        using FCompare = NodeFunctor < bool(const ObjectRef &,
+                                            const ObjectRef &, IRComparator *) >;
+        static FCompare & vtable();
+        bool Compare(const NodeRef & lhs,
+                     const NodeRef & rhs)
+        {
+                static const FCompare & f = vtable();
+                if (lhs.defined() && rhs.defined())
+                {
+                        return f(lhs, rhs, this);
+                }
+                return false;
+        }
+        bool _Compare(const Variable * const lhs, const Variable * const rhs);
+        bool _Compare(const Call * const lhs, const Call * const rhs);
+        bool _Compare(const Add * const lhs, const Add * const rhs);
+        bool _Compare(const Sub * const lhs, const Sub * const rhs);
+        bool _Compare(const Mul * const lhs, const Mul * const rhs);
+        bool _Compare(const Div * const lhs, const Div * const rhs);
+        bool _Compare(const Reduce * const lhs, const Reduce * const rhs);
+        bool _Compare(const IntImm   * const lhs, const IntImm   * const rhs);
+        bool _Compare(const UIntImm  * const lhs, const UIntImm  * const rhs);
+        bool _Compare(const FloatImm * const lhs, const FloatImm * const rhs);
+};  // class IRComparator
 
 
 IRComparator::FCompare & IRComparator::vtable()
@@ -221,6 +257,10 @@ TVM_STATIC_IR_FUNCTOR(IRComparator, vtable)
 .DISPATCH_TO_COMPARE(IntImm)
 .DISPATCH_TO_COMPARE(UIntImm)
 .DISPATCH_TO_COMPARE(FloatImm);
+
+
+
+
 
 
 void CSE(const Tensor & src, Tensor * const ptgt)
--- src/pass/cse.h
+++ src/pass/cse.h
@@ -5,46 +5,10 @@
 #include <unordered_set>
 
 #include <tvm/tensor.h>
-#include <tvm/ir_visitor.h>
 
 
 namespace tvm {
 namespace ir {
-
-
-class IRComparator
-{
-private:
-        bool _Compare(const Array < Expr > & lhs, const Array < Expr > & rhs);
-        bool _Compare(const IterVar & lhs, const IterVar & rhs);
-        bool _Compare(const Array < IterVar > & lhs,
-                      const Array < IterVar > & rhs);
-public:
-        ~IRComparator() {}
-        using FCompare = NodeFunctor < bool(const ObjectRef &,
-                                            const ObjectRef &, IRComparator *) >;
-        static FCompare & vtable();
-        bool Compare(const NodeRef & lhs,
-                     const NodeRef & rhs)
-        {
-                static const FCompare & f = vtable();
-                if (lhs.defined() && rhs.defined())
-                {
-                        return f(lhs, rhs, this);
-                }
-                return false;
-        }
-        bool _Compare(const Variable * const lhs, const Variable * const rhs);
-        bool _Compare(const Call * const lhs, const Call * const rhs);
-        bool _Compare(const Add * const lhs, const Add * const rhs);
-        bool _Compare(const Sub * const lhs, const Sub * const rhs);
-        bool _Compare(const Mul * const lhs, const Mul * const rhs);
-        bool _Compare(const Div * const lhs, const Div * const rhs);
-        bool _Compare(const Reduce * const lhs, const Reduce * const rhs);
-        bool _Compare(const IntImm   * const lhs, const IntImm   * const rhs);
-        bool _Compare(const UIntImm  * const lhs, const UIntImm  * const rhs);
-        bool _Compare(const FloatImm * const lhs, const FloatImm * const rhs);
-};  // class IRComparator
 
 
 /// @brief  Common Subexpression Elimination (Top-Level Function Call)
