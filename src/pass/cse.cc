0a34a5d172982a090303c5967837dcac8526b27a
ArmageddonKnight <bojian.zheng@mail.utoronto.ca>
2020-08-13 03:54:05 -0400

    Implemented the function dispatch.

--- src/pass/cse.cc
+++ src/pass/cse.cc
@@ -7,6 +7,64 @@ namespace tvm {
 namespace ir {
 
 
+#define DISPATCH_TO_COMPARE(op)                                                 \
+        set_dispatch < op > ([](const ObjectRef & lhs,                          \
+                                const ObjectRef & rhs, IRComparator * v)        \
+                               -> bool                                          \
+        {                                                                       \
+                v->Compare_(static_cast < const op * > (lhs.get()),             \
+                            static_cast < const op * > (rhs.get()));            \
+        })
+
+TVM_STATIC_IR_FUNCTOR(IRComparator, vtable)
+.DISPATCH_TO_COMPARE(Variable)
+.DISPATCH_TO_COMPARE(LetStmt)
+.DISPATCH_TO_COMPARE(AttrStmt)
+.DISPATCH_TO_COMPARE(IfThenElse)
+.DISPATCH_TO_COMPARE(For)
+.DISPATCH_TO_COMPARE(Allocate)
+.DISPATCH_TO_COMPARE(Load)
+.DISPATCH_TO_COMPARE(Store)
+.DISPATCH_TO_COMPARE(Let)
+.DISPATCH_TO_COMPARE(Free)
+.DISPATCH_TO_COMPARE(Call)
+.DISPATCH_TO_COMPARE(Add)
+.DISPATCH_TO_COMPARE(Sub)
+.DISPATCH_TO_COMPARE(Mul)
+.DISPATCH_TO_COMPARE(Div)
+.DISPATCH_TO_COMPARE(Mod)
+.DISPATCH_TO_COMPARE(FloorDiv)
+.DISPATCH_TO_COMPARE(FloorMod)
+.DISPATCH_TO_COMPARE(Min)
+.DISPATCH_TO_COMPARE(Max)
+.DISPATCH_TO_COMPARE(EQ)
+.DISPATCH_TO_COMPARE(NE)
+.DISPATCH_TO_COMPARE(LT)
+.DISPATCH_TO_COMPARE(LE)
+.DISPATCH_TO_COMPARE(GT)
+.DISPATCH_TO_COMPARE(GE)
+.DISPATCH_TO_COMPARE(And)
+.DISPATCH_TO_COMPARE(Or)
+.DISPATCH_TO_COMPARE(Reduce)
+.DISPATCH_TO_COMPARE(Cast)
+.DISPATCH_TO_COMPARE(Not)
+.DISPATCH_TO_COMPARE(Select)
+.DISPATCH_TO_COMPARE(Ramp)
+.DISPATCH_TO_COMPARE(Shuffle)
+.DISPATCH_TO_COMPARE(Broadcast)
+.DISPATCH_TO_COMPARE(AssertStmt)
+.DISPATCH_TO_COMPARE(ProducerConsumer)
+.DISPATCH_TO_COMPARE(Provide)
+.DISPATCH_TO_COMPARE(Realize)
+.DISPATCH_TO_COMPARE(Prefetch)
+.DISPATCH_TO_COMPARE(Block)
+.DISPATCH_TO_COMPARE(Evaluate)
+.DISPATCH_TO_COMPARE(IntImm)
+.DISPATCH_TO_COMPARE(UIntImm)
+.DISPATCH_TO_COMPARE(FloatImm)
+.DISPATCH_TO_COMPARE(StringImm);
+
+
 void CSE(const Tensor & src, Tensor * const ptgt)
 {
         Tensor & tgt = *ptgt;
--- src/pass/cse.h
+++ src/pass/cse.h
@@ -32,57 +32,56 @@ public:
         }
 #define NOT_IMPLEMENTED                                                         \
         {                                                                       \
-                LOG(FATAL) << "Comparator has not been implemented for "        \
-                           << *lhs << " vs. " << *rhs;                          \
+                LOG(FATAL) << "Comparator has not been implemented";            \
         }
-        bool Compare_(const Variable * lhs, const Variable * rhs);
-        bool Compare_(const LetStmt * lhs, const LetStmt * rhs)   NOT_IMPLEMENTED
+        bool Compare_(const Variable * lhs, const Variable * rhs) NOT_IMPLEMENTED
+        bool Compare_(const LetStmt * lhs, const LetStmt * rhs) NOT_IMPLEMENTED
         bool Compare_(const AttrStmt * lhs, const AttrStmt * rhs) NOT_IMPLEMENTED
         bool Compare_(const IfThenElse * lhs,
-                      const IfThenElse * rhs);
-        bool Compare_(const For * lhs, const For * RHS);
-        bool Compare_(const Allocate * lhs, const Allocate * rhs);
-        bool Compare_(const Load * lhs, const Load * rhs);
-        bool Compare_(const Store * lhs, const Store * rhs);
-        bool Compare_(const Let * lhs, const Let * rhs);
-        bool Compare_(const Free * lhs, const Free * rhs);
-        bool Compare_(const Call * lhs, const Call * rhs);
+                      const IfThenElse * rhs) NOT_IMPLEMENTED
+        bool Compare_(const For * lhs, const For * RHS) NOT_IMPLEMENTED
+        bool Compare_(const Allocate * lhs, const Allocate * rhs) NOT_IMPLEMENTED
+        bool Compare_(const Load * lhs, const Load * rhs) NOT_IMPLEMENTED
+        bool Compare_(const Store * lhs, const Store * rhs) NOT_IMPLEMENTED
+        bool Compare_(const Let * lhs, const Let * rhs) NOT_IMPLEMENTED
+        bool Compare_(const Free * lhs, const Free * rhs) NOT_IMPLEMENTED
+        bool Compare_(const Call * lhs, const Call * rhs) NOT_IMPLEMENTED
         bool Compare_(const Add * lhs, const Add * rhs);
         bool Compare_(const Sub * lhs, const Sub * rhs);
         bool Compare_(const Mul * lhs, const Mul * rhs);
         bool Compare_(const Div * lhs, const Div * rhs);
         bool Compare_(const Mod * lhs, const Mod * rhs);
-        bool Compare_(const FloorDiv * lhs, const FloorDiv * rhs);
-        bool Compare_(const FloorMod * lhs, const FloorMod * rhs);
-        bool Compare_(const Min * lhs, const Min * rhs);
-        bool Compare_(const Max * lhs, const Max * rhs);
-        bool Compare_(const EQ * lhs, const EQ * rhs);
-        bool Compare_(const NE * lhs, const NE * rhs);
-        bool Compare_(const LT * lhs, const LT * rhs);
-        bool Compare_(const LE * lhs, const LE * rhs);
-        bool Compare_(const GT * lhs, const GT * rhs);
-        bool Compare_(const GE * lhs, const GE * rhs);
-        bool Compare_(const And * lhs, const And * rhs);
-        bool Compare_(const Or * lhs, const Or * rhs);
-        bool Compare_(const Reduce * lhs, const Reduce * rhs);
-        bool Compare_(const Cast * lhs, const Cast * rhs);
-        bool Compare_(const Not * lhs, const Not * rhs);
-        bool Compare_(const Select * lhs, const Select * rhs);
-        bool Compare_(const Ramp * lhs, const Ramp * rhs);
-        bool Compare_(const Shuffle * lhs, const Shuffle * rhs);
-        bool Compare_(const Broadcast * lhs, const Broadcast * rhs);
-        bool Compare_(const AssertStmt * lhs, const AssertStmt * rhs);
+        bool Compare_(const FloorDiv * lhs, const FloorDiv * rhs) NOT_IMPLEMENTED
+        bool Compare_(const FloorMod * lhs, const FloorMod * rhs) NOT_IMPLEMENTED
+        bool Compare_(const Min * lhs, const Min * rhs) NOT_IMPLEMENTED
+        bool Compare_(const Max * lhs, const Max * rhs) NOT_IMPLEMENTED
+        bool Compare_(const EQ * lhs, const EQ * rhs) NOT_IMPLEMENTED
+        bool Compare_(const NE * lhs, const NE * rhs) NOT_IMPLEMENTED
+        bool Compare_(const LT * lhs, const LT * rhs) NOT_IMPLEMENTED
+        bool Compare_(const LE * lhs, const LE * rhs) NOT_IMPLEMENTED
+        bool Compare_(const GT * lhs, const GT * rhs) NOT_IMPLEMENTED
+        bool Compare_(const GE * lhs, const GE * rhs) NOT_IMPLEMENTED
+        bool Compare_(const And * lhs, const And * rhs) NOT_IMPLEMENTED
+        bool Compare_(const Or * lhs, const Or * rhs) NOT_IMPLEMENTED
+        bool Compare_(const Reduce * lhs, const Reduce * rhs) NOT_IMPLEMENTED
+        bool Compare_(const Cast * lhs, const Cast * rhs) NOT_IMPLEMENTED
+        bool Compare_(const Not * lhs, const Not * rhs) NOT_IMPLEMENTED
+        bool Compare_(const Select * lhs, const Select * rhs) NOT_IMPLEMENTED
+        bool Compare_(const Ramp * lhs, const Ramp * rhs) NOT_IMPLEMENTED
+        bool Compare_(const Shuffle * lhs, const Shuffle * rhs) NOT_IMPLEMENTED
+        bool Compare_(const Broadcast * lhs, const Broadcast * rhs) NOT_IMPLEMENTED
+        bool Compare_(const AssertStmt * lhs, const AssertStmt * rhs) NOT_IMPLEMENTED
         bool Compare_(const ProducerConsumer * lhs,
-                      const ProducerConsumer * rhs);
-        bool Compare_(const Provide * lhs, const Provide * rhs);
-        bool Compare_(const Realize * lhs, const Realize * rhs);
-        bool Compare_(const Prefetch * lhs, const Prefetch * rhs);
-        bool Compare_(const Block * lhs, const Block * rhs);
-        bool Compare_(const Evaluate * lhs, const Evaluate * rhs);
-        bool Compare_(const IntImm * lhs, const IntImm * rhs);
-        bool Compare_(const UIntImm * lhs, const UIntImm * rhs);
-        bool Compare_(const FloatImm * lhs, const FloatImm * rhs);
-        bool Compare_(const StringImm * lhs, const StringImm * rhs);
+                      const ProducerConsumer * rhs) NOT_IMPLEMENTED
+        bool Compare_(const Provide * lhs, const Provide * rhs) NOT_IMPLEMENTED
+        bool Compare_(const Realize * lhs, const Realize * rhs) NOT_IMPLEMENTED
+        bool Compare_(const Prefetch * lhs, const Prefetch * rhs) NOT_IMPLEMENTED
+        bool Compare_(const Block * lhs, const Block * rhs) NOT_IMPLEMENTED
+        bool Compare_(const Evaluate * lhs, const Evaluate * rhs) NOT_IMPLEMENTED
+        bool Compare_(const IntImm * lhs, const IntImm * rhs) NOT_IMPLEMENTED
+        bool Compare_(const UIntImm * lhs, const UIntImm * rhs) NOT_IMPLEMENTED
+        bool Compare_(const FloatImm * lhs, const FloatImm * rhs) NOT_IMPLEMENTED
+        bool Compare_(const StringImm * lhs, const StringImm * rhs) NOT_IMPLEMENTED
 };  // class IRComparator
 
 
