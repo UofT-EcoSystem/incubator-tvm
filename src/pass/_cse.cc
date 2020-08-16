a1c342abb5ad3b8e406e3bdd61d4d1a81885976c
ArmageddonKnight <bojian.zheng@mail.utoronto.ca>
2020-08-16 17:36:56 -0400

    Changed the logging format.

--- src/pass/_cse.cc
+++ src/pass/_cse.cc
@@ -477,11 +477,12 @@ struct TensorExpr
         std::string toString(const unsigned indent = 0)
         {
                 std::ostringstream strout;
-                strout << "\n" << op;
+                strout << "\n";
                 for (unsigned i = 0; i < indent; ++i)
                 {
                         strout << " ";
                 }
+                strout << op;
                 for (const auto & operand : operands)
                 {
                         strout << operand->toString(indent + 2);
