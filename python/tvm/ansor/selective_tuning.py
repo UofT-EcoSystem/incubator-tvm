dc91620782909e82f98a047bd77bfc7f3b301177
ArmageddonKnight <bojian.zheng@mail.utoronto.ca>
2020-09-18 21:52:54 -0400

    Working on selective tuning.

---
+++ python/tvm/ansor/selective_tuning.py
@@ -0,0 +1,13 @@
+import logging
+
+
+
+
+
+def PSMClustering(search_tasks):
+    return None, None
+
+
+def MarkDepend(search_tasks):
+    logging.info("Marking dependent tasks")
+    centroids, labels = PSMClustering(search_tasks)
