1e97227a9c5af6687a745f0c6539e6e4ce8e38a1
ArmageddonKnight <bojian.zheng@mail.utoronto.ca>
2020-09-19 18:06:48 -0400

    Converting the search tasks to sketches.

--- python/tvm/ansor/selective_tuning.py
+++ python/tvm/ansor/selective_tuning.py
@@ -1,13 +1,39 @@
 import logging
 
+from .cost_model    import XGBModel
+from .auto_schedule import SketchSearchPolicy
+
+search_policy = SketchSearchPolicy(program_cost_model=XGBModel(seed=0),
+                                   seed=0)
 
 
+def ComputeSimilarity(taskA, taskB):
+    """
+    Compute the similarity between two tasks.
+
+    In the original implementation, we are allowed to compute the similarity
+    between two tasks if they share the same schedule space map. However, since
+    the notion of schedule space map no longer exists in Ansor, we have to
+    switch to 
+    """
+    pass
 
 
-def PSMClustering(search_tasks):
+def PSMClustering(sketches):
     return None, None
+
+
+def SearchTask2Sketch(search_task):
+    """
+    Transform a search task to sketch.
+    """
+    sketch = search_policy.generate_sketches(task=search_task)
+    logging.info("Search Task={}, Sketch={}"
+                 .format(search_task, sketch))
+    return sketch
 
 
 def MarkDepend(search_tasks):
     logging.info("Marking dependent tasks")
-    centroids, labels = PSMClustering(search_tasks)
+    sketches = [SearchTask2Sketch(task) for task in search_tasks]
+    centroids, labels = PSMClustering(sketches)
