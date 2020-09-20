eefb518de1d02784693b6ce7e7eaa5bfa16cb2e4
ArmageddonKnight <bojian.zheng@mail.utoronto.ca>
2020-09-20 18:23:21 -0400

    Simplified the loggings.

--- python/tvm/autotvm/selective_tuning.py
+++ python/tvm/autotvm/selective_tuning.py
@@ -16,6 +16,5 @@ def PSMClustering(search_tasks):
 def MarkDepend(search_tasks):
     logger.info("Marking dependent tasks")
     for task in search_tasks:
-        logger.info("Config Space={}".format(task.config_space))
-        logger.info("Config Space.Space Map={}".format(task.config_space.space_map))
+        logger.info(task.config_space)
     centroids, labels = PSMClustering(search_tasks)
