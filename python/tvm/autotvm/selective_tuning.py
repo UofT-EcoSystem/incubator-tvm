3fac5ed5d091ea17f5e2fda8d818459a37d1a5bb
ArmageddonKnight <bojian.zheng@mail.utoronto.ca>
2020-09-20 18:56:59 -0400

    Added the PSM initialization.

--- python/tvm/autotvm/selective_tuning.py
+++ python/tvm/autotvm/selective_tuning.py
@@ -2,17 +2,32 @@
 <bojian/TVM-SymbolicTuning> Cluster search tasks for selective tuning (AutoTVM implementation).
 """
 import logging
+import numpy as np
 logger = logging.getLogger(__name__)
 
 
-def ComputeSimilarity(taskA, taskB):
-    pass
+def ComputePairwiseSimilarity(taskA, taskB):
+    """
+    Compare the pairwise similarity metric between two tasks.
+    """
+    if taskA.name != taskB.name:
+        return 0.
+    return 1.
+    
+
+def ComputePSM(search_tasks):
+    psm = np.zeros(shape=(len(search_tasks), len(search_tasks)),
+                   dtype=np.float32)
+    for i, _ in enumerate(search_tasks):
+        for j in range(i + 1, len(search_tasks)):
+            psm[i, j] = ComputePairwiseSimilarity(search_tasks[i], search_tasks[j])
 
 
-def PSMClustering(search_tasks):
+def ClusterPSM(search_tasks):
+    ComputePSM(search_tasks)
     return None, None
 
 
 def MarkDepend(search_tasks):
     logger.info("Marking dependent tasks")
-    centroids, labels = PSMClustering(search_tasks)
+    centroids, labels = PSMCluster(search_tasks)
